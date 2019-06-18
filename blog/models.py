d1a338df0be129bef0488a1f9380705d44acec5c
Froggy <froggling@yandex.ru>
2019-06-19 01:12:09 +1000

    signing up

--- blog/models.py
+++ blog/models.py
@@ -1,10 +1,12 @@
 from django.db import models
 from django.conf import settings
 from django.utils import timezone
+from django.contrib.auth import models as m
 
 
 class Post(models.Model):
-    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    author = models.ForeignKey(m.User, on_delete=models.CASCADE)
+   #author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     text = models.TextField()
     created_date = models.DateTimeField(blank=True, null=True)
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,10 @@
-from django.shortcuts import render, get_object_or_404
+from django.utils import timezone
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Post
+from django.contrib.auth.forms import UserCreationForm
+from .forms import PostForm
+from django.contrib.auth import authenticate, login
+from django.template import RequestContext
 
 
 def post_list(request):
@@ -10,3 +15,31 @@ def post_list(request):
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
+
+
+def post_new(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form': form})
+
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            my_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=my_password)
+            login(request, user)
+            return redirect('/')
+    else:
+        form = UserCreationForm()
+    return render(request, 'blog/signup.html', {'form': form})
